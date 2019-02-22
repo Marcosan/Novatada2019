f4448ce64d85184d58ef39266922290bc66f7e94
Christian Garcia <chadgarc@fiec.espol.edu.ec>
2019-02-21 19:31:15 -0500

    [UPD] Habilitado el cambio de los botones de interaccion

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -10,12 +10,23 @@ public class Destroyable : MonoBehaviour {
     public string destroyState;
     // Variable con los segundos a esperar antes de desactivar la colisión
     public float timeForDisable;
+    // Variable con el rango de vision
+    public float visionRadius;
 
     // Animador para controlar la animación
     Animator anim;
 
+    // Variable para guardar al jugador
+    GameObject player;
+
+    Vector3 forward;
+    RaycastHit2D hit;
+
     void Start () {
         anim = GetComponent<Animator>();
+
+        // Para recuperar al player
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Detectamos la colisión con una corrutina
@@ -37,6 +48,13 @@ public class Destroyable : MonoBehaviour {
 
     }
 
+    // Podemos dibujar el radio de visión y ataque sobre la escena dibujando una esfera
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
+    }
+
     void Update () {
         
         // "Destruir" el objeto al finalizar la animación de destrucción
@@ -46,9 +64,39 @@ public class Destroyable : MonoBehaviour {
         //stateInfo.normalizedTime >= 1 porque significa que termino la animacion, de [0,1]
         if (stateInfo.IsName(destroyState) && stateInfo.normalizedTime >= 1) {
             Destroy(gameObject);
+            changer.changeActionBtn(false);
             // En el futuro podríamos almacenar la instancia y su transform
             // para crearlos de nuevo después de un tiempo
         }
+
+        // Raycast desde el jarron hasta el player
+        hit = Physics2D.Raycast(
+            transform.position,
+            player.transform.position - transform.position,
+            visionRadius,
+            1 << LayerMask.NameToLayer("Default")
+            // Poner el propio Enemy en una layer distinta a Default para evitar el raycast
+            // También poner al objeto Attack y al Prefab Slash una Layer Attack 
+            // Sino los detectará como entorno y se mueve atrás al hacer ataques
+            );
+       
+         // Aquí podemos debugear el Raycast
+         forward = transform.TransformDirection(player.transform.position - transform.position);
+         Debug.DrawRay(transform.position, forward, Color.red);
+
+         // Si el Raycast encuentra al jugador habilitamos el boton
+         if (hit.collider != null)
+         {
+             if (hit.collider.tag == player.tag)
+             {
+                 changer.changeActionBtn(true); // Hace que el boton se habilite
+                 changer.setTag("Destroy"); // Hace que aparezca la imagen de acuerdo al tipo de interaccion
+             }
+         }
+         else
+         {
+             changer.changeActionBtn(false); // Deshabilita el boton
+         }
 
     }
 
