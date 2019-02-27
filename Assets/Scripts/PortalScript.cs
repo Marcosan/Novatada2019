fed8421a7bc9429354b09df5ba294ad67d90e990
David <davispalm@live.com.mx>
2019-02-27 03:09:29 -0500

    Animacion de portales en los clubes: Scene1

---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+
+
+
+
+
+    private Animator animacion;
+    private void Start()
+    {
+        animacion = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+         
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+            animacion.SetFloat("Entrada", 1);
+            animacion.SetFloat("Salida", 0);
+            Debug.Log("Esta entrando!");
+
+
+
+        }
+        else {
+
+            animacion.SetFloat("Entrada", 0);
+        }
+
+    }   
+
+    private void OnTriggerStay2D(Collider2D collision)
+              {
+
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+            animacion.SetFloat("Estadia", 1);
+            Debug.Log("Permanece!");
+
+
+        }
+        else {
+
+            animacion.SetFloat("Estadia", 0);
+
+        }
+
+
+
+              }
+
+              void OnTriggerExit2D(Collider2D collision)
+              {
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            animacion.SetFloat("Salida", 1);
+            animacion.SetFloat("Entrada", 0);
+            animacion.SetFloat("Estadia", 0);
+            Debug.Log("Se va!!");
+
+        }
+        else {
+
+            animacion.SetFloat("Salida", 0);
+        }
+
+
+
+              }
+    
+    
+
+
+
+}
