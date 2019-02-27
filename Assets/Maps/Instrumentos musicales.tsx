f46e69f1ab553ceded252cf3b3e959c93e2765ee
Nicole Alvarez <ngalvare>
2019-02-27 15:34:51 -0500

    [ADD]  Traslado entre zonas

---
+++ Assets/Maps/Instrumentos musicales.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Instrumentos musicales" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="Sptites/Instrumentos musicales.png" width="512" height="512"/>
+</tileset>
--- Assets/Maps/tsx/cosas 3.tsx
+++ Assets/Maps/tsx/cosas 3.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="cosas 3" tilewidth="16" tileheight="16" tilecount="1824" columns="38">
+ <grid orientation="orthogonal" width="32" height="32"/>
  <image source="../Sptites/2u9khw5.png" width="611" height="770"/>
 </tileset>
