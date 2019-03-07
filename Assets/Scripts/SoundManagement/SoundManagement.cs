fd2db146c54c57e1a6817f09f06fb6e03b31401c
Christian Garcia <chadgarc@fiec.espol.edu.ec>
2019-03-07 09:14:40 -0500

    [ADD] Agregada la musica de fondo
    Todo se encuentra en game canvas

---
+++ Assets/Scripts/SoundManagement/SoundManagement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SoundManagement : MonoBehaviour
+{
+    public static AudioSource musicPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicPlayer = GetComponent<AudioSource>();
+        musicPlayer.Play();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
