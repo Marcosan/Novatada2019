24d27780ce91c7b3d60966e5a0285a44f416414f
Nicole Alvarez <ngalvare@hotmai.com>
2019-03-14 15:28:16 -0500

    [NEW]Se creo mapa de minijuego laberinto
    ACP

---
+++ Assets/Maps/tsx/Instrumentos musicales.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Cerca01" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="../../../../../../Downloads/SPRITES/Cerca01.png" width="384" height="384"/>
+</tileset>
