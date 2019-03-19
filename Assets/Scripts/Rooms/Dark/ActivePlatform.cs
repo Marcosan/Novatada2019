4ea41e9bb4f3c53bff32bddc586ab80beabc461b
Marco <mmendozaquelal@gmail.com>
2019-03-19 11:31:14 -0500

    [NEW] Profundidad Mapa Kokoa

---
+++ Assets/Scripts/Rooms/Dark/ActivePlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ActivePlatform : MonoBehaviour {
+
+    public GameObject walls;
+    public TilemapRenderer tilemapRenderer;
+    public bool isActivator;
+
+    private void Start() {
+        GetComponent<SpriteRenderer>().enabled = false;
+        foreach (Transform child in walls.transform) {
+            child.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "Player") {
+            if (isActivator) {
+                foreach (Transform child in walls.transform) {
+                    child.GetComponent<BoxCollider2D>().enabled = true;
+                }
+                tilemapRenderer.sortingLayerName = "Default";
+            } else {
+                foreach (Transform child in walls.transform) {
+                    child.GetComponent<BoxCollider2D>().enabled = false;
+                }
+                tilemapRenderer.sortingLayerName = "Ceilings";
+            }
+        }
+    }
+}
